2881645fc2cabd7776bbf67f3390356d1ca482af
Miguel Di Ciurcio Filho <miguel@instruct.com.br>
2019-05-21 16:27:27 -0300

    Initial release

---
+++ README.reduced.md
@@ -0,0 +1,20 @@
+# Reduced Puppet stdlib module
+
+*WARNING: This is VERY experimental and VERY opinionated*
+
+This is an attempt to reduce the footprint of the traditional Puppet module `stdlib`.
+
+We remove most of the deprecated functions and much more.
+
+Check the `deprecated-functions.txt` file on this repository and also the `reducer.sh` script.
+
+The resulting module supports Puppet 5.5 or newer.
+
+## TODO
+
+Some deprecated functions are used by unit tests and are still present:
+
+* is_integer
+* is_absolute_path
+* str2bool
+
---
+++ reducer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+REDUCED_REMOTE="git@github.com:instruct-br/puppet-reduced_stdlib.git"
+CURRENT_DATE=$(date)
+
+rm -rf puppetlabs-stdlib
+
+git clone https://github.com/puppetlabs/puppetlabs-stdlib.git
+
+cd puppetlabs-stdlib || exit
+
+GIT_COMMIT=$(git rev-parse HEAD)
+
+git rm -r lib/facter
+git rm -r spec/unit/facter/
+git rm -r locales
+
+# shellcheck disable=SC2002
+cat ../deprecated-functions.txt | while read -r f; do
+  git rm "lib/puppet/functions/${f}.rb"
+  git rm "lib/puppet/parser/functions/${f}.rb"
+  git rm "spec/functions/${f}_spec.rb"
+done
+
+git rm lib/puppet/type/anchor.rb
+git rm spec/unit/puppet/type/anchor_spec.rb
+git rm spec/acceptance/anchor_spec.rb
+
+git mv README.md README.original.md
+
+cp ../README.reduced.md README.md
+
+git add README.md
+
+git commit -a -m "Reduced commit ${GIT_COMMIT} on ${CURRENT_DATE}"
+git remote rm origin
+git remote add reduced $REDUCED_REMOTE
+
